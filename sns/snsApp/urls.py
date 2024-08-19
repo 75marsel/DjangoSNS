f7e77c242b0b68e594fdf47a88302f5f7086f4d2
75marsel <jericgappi@gmail.com>
2024-08-19 22:07:06 +0800

    added some profile list

--- sns/snsApp/urls.py
+++ sns/snsApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_view, name="home")
+    path("", views.home_view, name="home"),
+    path("profile_list/", views.profile_list_view, name="profile_list"),
 ]
--- sns/snsApp/views.py
+++ sns/snsApp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from .models import Profile
 
 def home_view(request):
     return render(
         request,
         "snsApp/home.html",
         {}
+    )
+    
+def profile_list_view(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    
+    context = {
+        "profiles": profiles
+    }
+    
+    return render(
+        request,
+        "snsApp/profile_list.html",
+        context,
     )
